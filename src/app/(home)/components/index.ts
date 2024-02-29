485f3d90634473dc10cedf6e682512be17d9f2dd
Shukurullox <109385687+shukurullox0507@users.noreply.github.com>
2024-02-29 12:10:12 +0500

    main page finished

---
+++ src/app/(home)/components/index.ts
@@ -0,0 +1,9 @@
+import { Footer } from './footer';
+import { PricingCard } from './pricingCard';
+import { Pricing } from './pricing';
+import { Clients } from './clients';
+import { Heroes } from './heroes';
+import { Logo } from './logo';
+import { Navbar } from "./navbar"
+
+export { Navbar, Logo, Heroes, Clients, Pricing, PricingCard, Footer };
